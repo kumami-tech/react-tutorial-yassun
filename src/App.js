bb1a11d0241a008bf1efbb2f55f92a52d63896ac
s-kumagai <sk924731@gmail.com>
2021-06-06 21:27:23 +0900

    タイトルとフォームのタブを追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { List } from "./List";
+import { Form } from "./Form";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { description: "クリック前の表示" }
+    this.state = {
+      description: "クリック前の表示",
+      tab: 'list',
+    }
   }
   changeDescription() {
     this.setState({
@@ -12,11 +16,32 @@ class App extends React.Component {
     })
   }
   render() {
-    const { description } = this.state;
+    const { tab, description } = this.state;
     return (
       <div>
+        <header>
+          <ul>
+            <li
+              onClick={() =>
+                this.setState({tab: 'list'})}
+            >
+              リスト
+            </li>
+            <li
+              onClick={() =>
+                this.setState({tab: 'form'})}
+            >
+              フォーム
+            </li>
+          </ul>
+        </header>
+        <hr />
         { description }
-        <List title="取り扱い言語一覧" />
+        {
+          tab === "list" ?
+            <List title="取り扱い言語一覧" /> :
+            <Form />
+        }
         <button onClick={() => this.changeDescription()}>
           ボタン
         </button>
