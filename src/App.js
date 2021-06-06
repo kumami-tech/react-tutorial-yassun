d1a59da054ff061777dba562f05472894b03007b
s-kumagai <sk924731@gmail.com>
2021-06-06 21:13:30 +0900

    descriptionとtitleを削除

--- src/App.js
+++ src/App.js
@@ -3,26 +3,20 @@ import { List } from "./List";
 import { Form } from "./Form";
 
 function App() {
-  const [description, setDescription] = useState('クリック前の表示');
   const [tab, setTab] = useState("list");
-  const changeDescription = () => {
-    setDescription('クリック後の表示です。')
-  }
 
   return (
     <div>
       <header>
-        <ul>
+        <ul>　
           <li onClick={() => setTab("list")}>リスト</li>
           <li onClick={() => setTab("form")}>フォーム</li>
         </ul>
       </header>
       <hr />
-      { description }
       {
-        tab === "list" ? <List title="取り扱い言語一覧" /> : <Form />
+        tab === "list" ? <List /> : <Form />
       }
-      <button onClick={changeDescription}>ボタン</button>
     </div>
   );
 }
