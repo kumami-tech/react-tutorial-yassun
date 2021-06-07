9f4a8f06634cd6889c4945682eae18b488530e86
s-kumagai <sk924731@gmail.com>
2021-06-07 22:48:49 +0900

    App.jsとList.jsとのデータの受け渡しによるフォームを実装

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { List } from "./List";
 import { Form } from "./Form";
+import { LANGUAGES } from "./const/languages";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       tab: 'list',
+      langs: LANGUAGES
     }
   }
+  addLang(lang) {
+    this.setState({
+      tab: 'list',
+      langs: [...this.state.langs, lang]
+    });
+  }
   render() {
-    const { tab } = this.state;
+    const { tab, langs } = this.state;
     return (
       <div>
         <header>
@@ -32,8 +40,8 @@ class App extends React.Component {
         <hr />
         {
           tab === "list" ?
-            <List /> :
-            <Form />
+            <List langs={langs} /> :
+            <Form onAddLang={(lang) => this.addLang(lang)} />
         }
       </div>
     )
