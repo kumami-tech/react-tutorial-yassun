8c8833be7eeb060c9da8e5de221ac65dedc70183
s-kumagai <sk924731@gmail.com>
2021-06-18 07:29:37 +0900

    ヒントを追加

--- src/Form.js
+++ src/Form.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { Button } from './components/button';
 import { TabBodyContainer } from './components/tab-body-container';
 import { FormModal } from './FormModal';
+import { Hint } from './Hint';
 
 const Label = styled.label`
   display: flex;
@@ -41,6 +42,7 @@ export const Form = ({ onAddLang }) => {
             value={text}
             onChange={(e) => setText(e.target.value)}
           />
+          <Hint />
         </div>
         <ButtonContainer>
           <FormButton>追加</FormButton>
